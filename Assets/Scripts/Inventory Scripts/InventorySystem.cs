aac453c28b702e7e3ec5c2a324443151331ceb9c
ErenYZR <erenyazar16@gmail.com>
2024-10-11 18:38:07 +0300

    Inventory Backend done

--- Assets/Scripts/Inventory Scripts/InventorySystem.cs
+++ Assets/Scripts/Inventory Scripts/InventorySystem.cs
@@ -22,4 +22,41 @@ public class InventorySystem
 			inventorySlots.Add(new InventorySlot());
         }
     }
+
+	public bool AddToInventory(InventoryItemData itemToAdd, int amountToAdd)
+	{
+		if(ContainsItem(itemToAdd, out List<InventorySlot> invSlot)) // Check whether item exist in inventory.
+		{
+            foreach (var slot in invSlot)
+            {
+				if (slot.RoomLeftInStack(amountToAdd))
+				{
+					slot.AddToStack(amountToAdd);
+					OnInventorySlotChanged?.Invoke(slot);
+					return true;
+				}
+            }
+		}
+				
+		if(HasFreeSlot(out InventorySlot freeSlot))// Gets the first available slot.
+		{
+			freeSlot.UpdateInventorySlot(itemToAdd, amountToAdd);
+			OnInventorySlotChanged?.Invoke(freeSlot);
+			return true;
+		}
+
+		return false;
+	}
+	public bool ContainsItem(InventoryItemData itemToAdd, out List<InventorySlot> invSlot)
+	{
+		invSlot = InventorySlots.Where(i => i.ItemData == itemToAdd).ToList();
+
+		return invSlot == null ? false : true;
+	}
+
+	public bool HasFreeSlot(out InventorySlot freeSlot)
+	{
+		freeSlot = InventorySlots.FirstOrDefault(i => i.ItemData == null);
+		return freeSlot == null ? false : true;
+	}
 }
